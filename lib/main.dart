977a1aab4666ebd53ce18569fd7d56f82c67f2d3
nekoritsme <89358408+nekoritsme@users.noreply.github.com>
2026-02-07 21:28:14 +0100

    Talker zone and basic theme init

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,33 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:talker_flutter/talker_flutter.dart';
+
+void main() {
+  final talker = Talker();
+
+  runTalkerZonedGuarded(
+    talker,
+    () {
+      WidgetsFlutterBinding.ensureInitialized();
+      runApp(ChatApp());
+    },
+    (error, stackTrace) {
+      talker.error(error, stackTrace);
+    },
+  );
+}
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromARGB(1, 19, 236, 1),
+        ),
+      ),
+      home: const Placeholder(),
+    );
+  }
+}
